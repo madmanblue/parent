c16636dde7d544be4f2e10dcd71293acb4e8879e
sunjianfeng <sunjianfeng@baozheng.cc>
2020-08-06 13:48:12 +0800

    json

---
+++ src/main/java/com/blue/util/JsonUtil.java
@@ -0,0 +1,39 @@
+package com.blue.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+
+public class JsonUtil {
+
+    public static String obj2Str(Object o) {
+        if (null == o) {
+            return null;
+        }
+
+        ObjectMapper mp = new ObjectMapper();
+        mp.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            return mp.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static <T> T json2Obj(String json, T t) {
+        if (StringUtils.isEmpty(json) || null == t) {
+            return null;
+        }
+        ObjectMapper mp = new ObjectMapper();
+        mp.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            return (T) mp.readValue(json, t.getClass());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
